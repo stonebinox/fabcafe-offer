e5d30e3fe73edc0d2a1506f2c115941a7d3c1b41
Anoop Santhanam <anoop@playerslounge.co>
2019-11-07 18:24:30 +0530

    Modified instructions

--- src/components/Instructions/Instructions.js
+++ src/components/Instructions/Instructions.js
@@ -10,8 +10,8 @@ export class Instructions extends Component {
                     <div className="col-sm-4">
                         <h3 className="text-center">Instructions to get this:</h3>
                         <ul className="list-group">
-                            <li className="list-group-item">Take a selfie or a photo and mention us/tag us in your Instagram story or photo.</li>
-                            <li className="list-group-item">This offer is valid for mininmum purchases of Rs. 250 and above.</li>
+                            <li className="list-group-item">Take a selfie or a photo and mention us/tag us in your Instagram/Facebook story or photo.</li>
+                            <li className="list-group-item">This offer is valid for mininimum purchases of Rs. 250 and above.</li>
                         </ul>
                     </div>
                 </div>
